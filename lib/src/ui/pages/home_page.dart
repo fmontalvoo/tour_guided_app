a3adc0d5f586e1cad3d4d8b682f26d2fc84dda19
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-04-23 13:14:29 -0500

    Initial commit
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- lib/src/ui/pages/home_page.dart
+++ lib/src/ui/pages/home_page.dart
@@ -1,51 +1,25 @@
 import 'package:flutter/material.dart';
 
 import 'package:showcaseview/showcaseview.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:tour_guided_app/src/ui/widgets/category_list_item.dart';
+
+import 'package:tour_guided_app/src/utils/perfs.dart';
 import 'package:tour_guided_app/src/utils/categories.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
+// ignore: must_be_immutable
+class HomePage extends StatelessWidget {
   GlobalKey _one = GlobalKey();
   GlobalKey _two = GlobalKey();
   GlobalKey _three = GlobalKey();
   GlobalKey _four = GlobalKey();
   GlobalKey _five = GlobalKey();
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   WidgetsBinding.instance.addPostFrameCallback((_) =>
-  //       ShowCaseWidget.of(context)
-  //           .startShowCase([_one, _two, _three, _four, _five]));
-  // }
+  final _prefs = Prefs();
 
   @override
   Widget build(BuildContext context) {
-    SharedPreferences preferences;
-
-    displayShowcase() async {
-      preferences = await SharedPreferences.getInstance();
-      bool showcaseVisibilityStatus = preferences.getBool("showShowcase");
-      if (showcaseVisibilityStatus == null) {
-        preferences.setBool("showShowcase", false).then((bool success) {
-          if (success)
-            print("Se escribió en showShowcase");
-          else
-            print("Ocurrio un problema");
-        });
-        return true;
-      }
-      return false;
-    }
-
-    displayShowcase().then((status) {
+    _prefs.displayShowcase().then((status) {
       if (status) {
         ShowCaseWidget.of(context)
             .startShowCase([_one, _two, _three, _four, _five]);
@@ -53,78 +27,88 @@ class _HomePageState extends State<HomePage> {
     });
 
     return Scaffold(
-      body: SafeArea(
-          child: ListView(
-        children: [
-          Container(
-              margin: EdgeInsets.all(30.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      appBar: appBar(),
+      body: body(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 1.0,
+      leading: Showcase(
+        key: _one,
+        description: "Desde aqui tienes acceso al menu",
+        child: IconButton(
+          icon: Icon(Icons.menu, color: Colors.black87),
+          onPressed: () {},
+        ),
+      ),
+      actions: [
+        Showcase(
+            key: _two,
+            title: "Contador",
+            description: "Aqui puedes revisar el contador",
+            showcaseBackgroundColor: Colors.blue[100],
+            child: IconButton(
+              icon: Icon(Icons.countertops_rounded, color: Colors.black87),
+              onPressed: () {},
+            )),
+      ],
+    );
+  }
+
+  SafeArea body() {
+    return SafeArea(
+        child: ListView(
+      children: [
+        Container(
+            margin: EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 30.0),
+                Showcase.withWidget(
+                  key: _three,
+                  width: 140.0,
+                  height: 50.0,
+                  shapeBorder: CircleBorder(),
+                  container: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Showcase(
-                          key: _one,
-                          description: "Desde aqui tienes acceso al menu",
-                          child: Container(child: Icon(Icons.menu))),
-                      Showcase(
-                        key: _two,
-                        title: "Conador",
-                        description: "Aqui puedes revisar el contador",
-                        showcaseBackgroundColor: Colors.blue[100],
-                        child: Container(
-                            padding: EdgeInsets.all(15),
-                            decoration: BoxDecoration(
-                                color: Colors.blue[400],
-                                borderRadius: BorderRadius.circular(50.0)),
-                            child: Text('0')),
-                      )
+                      Container(
+                        width: 45.0,
+                        height: 45.0,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.blue[400]),
+                        child: Icon(Icons.text_fields),
+                      ),
+                      SizedBox(height: 10),
+                      Text("Aqui esta el titulo de la aplicacion",
+                          style: TextStyle(color: Colors.white))
                     ],
                   ),
-                  SizedBox(height: 30.0),
-                  Showcase.withWidget(
-                    key: _three,
-                    width: 140.0,
-                    height: 50.0,
-                    shapeBorder: CircleBorder(),
-                    container: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          width: 45.0,
-                          height: 45.0,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle, color: Colors.blue[400]),
-                          child: Icon(Icons.text_fields),
-                        ),
-                        SizedBox(height: 10),
-                        Text("Aqui esta el titulo de la aplicacion",
-                            style: TextStyle(color: Colors.white))
-                      ],
-                    ),
-                    child: title(),
-                  ),
-                  SizedBox(height: 30.0),
-                  Showcase(
-                    key: _four,
-                    description: "Aqui puedes buscar",
-                    descTextStyle: TextStyle(
-                        color: Colors.green, fontWeight: FontWeight.bold),
-                    child: searchBox(),
-                  ),
-                  SizedBox(height: 30.0),
-                  Showcase(
-                    key: _five,
-                    description: "Escoge una categoria",
-                    child: categories(),
-                  )
-                ],
-              ))
-        ],
-      )),
-    );
+                  child: title(),
+                ),
+                SizedBox(height: 30.0),
+                Showcase(
+                  key: _four,
+                  description: "Aqui puedes buscar",
+                  descTextStyle: TextStyle(
+                      color: Colors.green, fontWeight: FontWeight.bold),
+                  child: searchBox(),
+                ),
+                SizedBox(height: 30.0),
+                Showcase(
+                  key: _five,
+                  description: "Escoge una categoria",
+                  child: categories(),
+                )
+              ],
+            ))
+      ],
+    ));
   }
 
   Widget title() {
---
+++ lib/src/utils/perfs.dart
@@ -0,0 +1,17 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Prefs {
+  SharedPreferences preferences;
+
+  Future<bool> displayShowcase() async {
+    preferences = await SharedPreferences.getInstance();
+    bool showShowcase = preferences.getBool("showShowcase");
+    if (showShowcase != null) {
+      if (showShowcase) return true;
+    } else {
+      final status = await preferences.setBool("showShowcase", false);
+      return status;
+    }
+    return false;
+  }
+}
